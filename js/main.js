b5f3087047ecbda21d9c9fb7ada7fc0f7e1eb708
Saunders <loganes1@byui.edu>
2019-06-28 18:40:28 -0600

    testing

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+function loadNavBar() {
+	$("nav").load("../html/nav-bar.html");
+}
+
 function showSteps() {
 	var steps = document.getElementsByClassName("hidden_1");
-	for (var i = 0 ; i < steps.length; ++i) {
+	for (var i = 0; i < steps.length; ++i) {
 		if (steps[i].style.display == "none") {
 			steps[i].style.display = "block";
-		}
-		else {
+		} else {
 			steps[i].style.display = "none";
-		} 
+		}
 	}
 }
