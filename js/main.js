a85463b8375f6a50c3c94dc664d57ac967e3269d
Saunders <loganes1@byui.edu>
2019-06-28 18:45:56 -0600

    removed loading the anve bar dynamically because it can only be done using a server

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-function loadNavBar() {
-	$("nav").load("../html/nav-bar.html");
-}
-
 function showSteps() {
 	var steps = document.getElementsByClassName("hidden_1");
 	for (var i = 0; i < steps.length; ++i) {
