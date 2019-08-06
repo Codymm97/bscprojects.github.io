0606c0df6443c95635bc424a21d62e07e68e7361
Saunders <loganes1@byui.edu>
2019-08-06 11:11:28 -0600

    started documentation for the quiq automation

---
+++ quiq-automation/js/nav.js
@@ -0,0 +1,27 @@
+// Scroll to show where your active link is
+window.onscroll = (e) => {
+    const elements = document.getElementsByClassName("content-section");
+    for (let i = 0; i < elements.length; i++) {
+        if (document.documentElement.scrollTop >= elements[i].offsetTop) //Adjust Tolerance as you want
+        {
+            const activeElmnt = document.getElementsByClassName("active")[0]; // there should only be one elmnt with this class
+            // remove the active class from the link
+            activeElmnt.classList.remove("active");
+            // add the active class to this link
+            const navLinks = document.getElementsByClassName("nav-link");
+            for (let j = 0; j < navLinks.length; j++) {
+                if (navLinks[j].innerHTML.toLowerCase() == elements[i].id) navLinks[j].classList.add("active");
+            }
+        }
+    }
+}
+
+// smooth scrolling when you click on an anchor
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
